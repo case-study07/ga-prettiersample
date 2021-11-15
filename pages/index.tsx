210a68f209a4a0ebce18ca0c895195449e738023
piyoraik <piyoraik@users.noreply.github.com>
2021-11-15 05:40:35 +0000

    Apply Prettier Change

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,9 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  const adsfsa = () => {console.log('dsfaklds')}
+  const adsfsa = () => {
+    console.log('dsfaklds')
+  }
   const aaa = () => {
     console.log('aaa')
   }
